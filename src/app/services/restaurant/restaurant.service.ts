91f1e706839b37559d941e0715d556952b091a1e
Miroslav Foltýn <miroslav.foltyn64@gmail.com>
2021-09-23 14:56:22 +0200

    wip

--- src/app/services/restaurant/restaurant.service.ts
+++ src/app/services/restaurant/restaurant.service.ts
@@ -1,11 +1,12 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { BehaviorSubject, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RestaurantService {
+  refreshLucky$ = new BehaviorSubject(void 0);
   constructor(private httpClient: HttpClient) {}
 
   restaurants(): Observable<any> {
