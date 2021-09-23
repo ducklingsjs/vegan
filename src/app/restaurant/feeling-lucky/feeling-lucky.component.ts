a9fa5ad1292d236bf911779026d5489765d49bfa
Miroslav Foltýn <miroslav.foltyn64@gmail.com>
2021-09-23 14:45:35 +0200

    wip

--- src/app/restaurant/feeling-lucky/feeling-lucky.component.ts
+++ src/app/restaurant/feeling-lucky/feeling-lucky.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { bufferCount, map, shareReplay, tap } from 'rxjs/operators';
 import { RestaurantService } from 'src/app/services/restaurant/restaurant.service';
 
 @Component({
@@ -9,7 +10,15 @@ import { RestaurantService } from 'src/app/services/restaurant/restaurant.servic
 })
 export class FeelingLuckyComponent {
 
-  restaurant$ = this.restaurant.restaurants();
+  restaurant$ = this.restaurant.restaurants().pipe(
+    map((restaurants) => restaurants.restaurants[Math.floor(Math.random() * restaurants.restaurants?.length)]),
+    shareReplay(1),
+  );
+
+  meal$ = this.restaurant$.pipe(
+    map((restaurant) => restaurant.meals[Math.floor(Math.random() * restaurant.meals?.length)]),
+    shareReplay(1),
+  );
 
   constructor(private restaurant: RestaurantService) { }
 }
