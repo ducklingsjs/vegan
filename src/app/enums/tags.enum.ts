f6e588b146623a94bbef7b08e86f712bd744362b
Tomislav Habalija <tomislav.habalija@infinum.hr>
2021-09-23 10:33:30 +0200

    Tags

---
+++ src/app/enums/tags.enum.ts
@@ -0,0 +1,37 @@
+// meal tag
+export enum DietType {
+    VEGAN = "VEGAN",
+    VEGETERIAN = "VEGETERIAN",
+}
+
+// restaurant tag
+export enum SpecialRestaurantTag {
+    BAMBILIKES = "BAMBILIKES",
+}
+
+// restaurant tag (for MVP)
+export enum Cousine {
+    ITALIAN = "ITALIAN",
+    KOREAN = "KOREAN",
+    MEXICAN = "MEXICAN",
+    CHINESE = "CHINESE",
+    INDIAN = "INDIAN",
+    BALKAN = "BALKAN",
+}
+
+// meal tag
+export enum Taste {
+    SPICY = "SPICY",
+}
+
+// meal tag
+export enum Price {
+    CHEAP = "CHEAP",
+    EXPENSIVE = "EXPENSIVE",
+    REGULAR = "REGULAR",
+}
+
+// meal tag
+export enum SpecialMealTag {
+    CHINESE_SOUP = "CHINESE_SOUP",
+}
